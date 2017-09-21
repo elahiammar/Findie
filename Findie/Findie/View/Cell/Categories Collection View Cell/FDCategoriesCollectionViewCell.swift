fd81c138e180a807259407d7c474193376a0cbfc
Muhammad Ammar Elahi <elahiammar@gmail.com>
2017-09-22 00:30:22 +0500

    Created classes for views

---
+++ Findie/Findie/View/Cell/Categories Collection View Cell/FDCategoriesCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  FDCategoriesCollectionViewCell.swift
+//  Findie
+//
+//  Created by elahiammar on 21/09/2017.
+//  Copyright © 2017 elahiammar. All rights reserved.
+//
+
+import UIKit
+
+class FDCategoriesCollectionViewCell: UICollectionViewCell {
+    
+}
