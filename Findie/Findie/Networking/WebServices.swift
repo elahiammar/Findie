8dd6e704a7eb1c732cf12542a1da8e0d055b333d
elahiammar <elahiammar@gmail.com>
2018-02-28 00:31:06 +0500

    Implementing MKLocalSearch api

--- Findie/Findie/Networking/WebServices.swift
+++ Findie/Findie/Networking/WebServices.swift
@@ -7,12 +7,22 @@
 //
 
 import Foundation
+import MapKit
 import CoreLocation
 
 struct WebServices: WebServicesProtocol {
     // Search Location
     func searchLocation(with locationName: String, completionHandler: @escaping CompletionHandler) {
-    
+        let coordinate = DataManager.shared.currentCoordinate
+        let request = MKLocalSearchRequest()
+        request.naturalLanguageQuery = locationName
+        request.region = MKCoordinateRegionMakeWithDistance(coordinate!, 1000, 1000)
+        
+        let search = MKLocalSearch(request: request)
+        search.start { (response, error) in
+            
+            
+        }
     }
     
 }
